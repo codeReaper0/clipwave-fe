9940f952a8a6531f25c06614641944c08699de57
codeReaper0 <boluwatifetella@gmail.com>
2025-08-15 23:14:49 +0100

    updated build folder

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
+  distDir: "build",
   images: {
     domains: ["res.cloudinary.com", "upload-widget.cloudinary.com"],
   },

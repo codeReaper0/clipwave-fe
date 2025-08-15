a1894b440c30fb5826e54e2025bc4170779be98e
codeReaper0 <boluwatifetella@gmail.com>
2025-08-15 23:29:15 +0100

    updated export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   distDir: "build",
+  output: "export",
+
   images: {
     domains: ["res.cloudinary.com", "upload-widget.cloudinary.com"],
   },
